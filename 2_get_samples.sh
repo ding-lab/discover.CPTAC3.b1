c95bee1138d21918ea8b29cedb48de10dea80b8a
Matthew Wyczalkowski <m.wyczalkowski@wustl.edu>
2017-12-27 12:18:57 -0600

    Functional queries for CPTAC3 Batch 2 samples
    * Paths are defined in a single file
    * specific modifications for B2 in 1_make_case_list.sh

--- 2_get_samples.sh
+++ 2_get_samples.sh
@@ -1,5 +1,6 @@
-TOKEN="token/gdc-user-token.2017-11-04T01-21-42.215Z.txt"
-SRC="/Users/mwyczalk/src/queryGDC/case.discover"
+source CPTAC3.b2.paths.sh
+
+SRC="queryGDC/case.discover"
 
 function process_case {
 DAT=$1
@@ -9,7 +10,7 @@ while read CASE; do
 [[ $CASE = \#* ]] && continue  # Skip commented out entries
 >&2 echo Processing $CASE
 
-bash $SRC/get_sample.sh $CASE $TOKEN
+bash "$QUERYGDC_HOME/case.discover/get_sample.sh" $CASE 
 
 done < $DAT
 }
---
+++ CPTAC3.b2.paths.sh
@@ -0,0 +1,4 @@
+# Define project paths and environment variables
+
+export GDC_TOKEN="/Users/mwyczalk/Data/CPTAC3/Discovery/discover.CPTAC3.b1/token/gdc-user-token.2017-12-27T17_47_44.208Z.txt"
+export QUERYGDC_HOME="./queryGDC"
