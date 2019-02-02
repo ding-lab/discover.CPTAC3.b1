c49f0117f07876edf92c0c203a0b5be6b89615b4
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2019-02-01 22:02:32 -0600

    Relaxing assumption SR are hg19, and making prior run optional
    * hg19->"sub", to indicate we do not know reference of submitted data

--- 2_process_cases.sh
+++ 2_process_cases.sh
@@ -1,12 +1,15 @@
 source discover.paths.sh
 
-if [ ! -d $OLDRUN ]; then
+>&2 echo Iterating over $DISCOVER_CASES
+if [ -z $OLDRUN ]; then
+    >&2 echo No prior runs
+elif [ ! -d $OLDRUN ]; then
     >&2 echo Old Run $OLDRUN does not exist
     exit 1
+else
+    >&2 echo Comparing with past run $OLDRUN
 fi
 
-echo Iterating over $DISCOVER_CASES
-echo Comparing with past run $OLDRUN
 
 # Note that since this takes some time to process, can run it in background as,
 # nohup bash 2_process_cases.sh &> 2.out &
@@ -36,25 +39,26 @@ function process_case {
     test_exit_status
 
     # Evaluate old vs. new to see if can short-circuit the get_submitted_reads.sh evaluation
-    OLD_RESULT="$OLDRUN/dat/$CASE/read_group_from_case.${CASE}.dat"
     NEW_RESULT="dat/$CASE/read_group_from_case.${CASE}.dat"
 
-    # if result file is empty, 
-
     SHORT_CIRCUIT=0
-    if [ -e $OLD_RESULT ]; then
-        OLDMD5=$(md5sum $OLD_RESULT | cut -f 1 -d ' ')
-        NEWMD5=$(md5sum $NEW_RESULT | cut -f 1 -d ' ')
-        printf "Comparing $OLD_RESULT and $NEW_RESULT  \n  " 1>&2
+    if [ ! -z $OLDRUN ]; then
+        OLD_RESULT="$OLDRUN/dat/$CASE/read_group_from_case.${CASE}.dat"
+        if [ -e $OLD_RESULT ]; then
+            OLDMD5=$(md5sum $OLD_RESULT | cut -f 1 -d ' ')
+            NEWMD5=$(md5sum $NEW_RESULT | cut -f 1 -d ' ')
+            printf "Comparing $OLD_RESULT and $NEW_RESULT  \n  " 1>&2
 
-        if [ "$OLDMD5" == "$NEWMD5" ]; then  # matching results.  Copy old to new 
-            SHORT_CIRCUIT=1
-            printf "... Match, will copy old results\n" 1>&2
-        else
-            printf "... Mismatch, re-evaluating results \n" 1>&2
-        fi
-    fi  
-
+            if [ "$OLDMD5" == "$NEWMD5" ]; then  # matching results.  Copy old to new 
+                SHORT_CIRCUIT=1
+                printf " OLDRUN Match, will copy old results\n" 1>&2
+            else
+                printf " OLDRUN Mismatch, re-evaluating results \n" 1>&2
+            fi
+        fi  
+    else
+        printf " OLDRUN not defined, evaluating results \n" 1>&2
+    fi
 
     if [ "$SHORT_CIRCUIT" == 1 ]; then
         OLD_RESULT="$OLDRUN/dat/$CASE/SR_from_read_group.$CASE.dat"
--- 4_make_HAR_report.sh
+++ 4_make_HAR_report.sh
@@ -1,6 +1,6 @@
 source discover.paths.sh
 
-SR="dat/$PROJECT.SR-hg19.dat"
+SR="dat/$PROJECT.SR-sub"
 HAR="dat/$PROJECT.HAR-hg38.dat"
 
 bash CPTAC3.case.discover/get_harmonized_reads.sh $SR $HAR
